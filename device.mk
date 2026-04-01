0a3bd5448926ee2fa4bc5ccdc680b205742f3d23
KSN2redawew <redawew9@gmail.com>
2026-04-01 21:50:07 +0300

    NX809J : initial twrp

---
+++ device.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright (C) 2024 The OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+LOCAL_PATH := device/redmagic/NX809J
+
+# Shipping API level
+BOARD_SHIPPING_API_LEVEL := 36
+PRODUCT_SHIPPING_API_LEVEL := 36
+PRODUCT_TARGET_VNDK_VERSION := 36
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
